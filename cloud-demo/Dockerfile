1e09af2a7dbf065e00c37c89ae3b0a3b8832bd20
whatsgoodg <rlwjddl1596@naver.com>
2024-01-30 12:47:40 +0000

    using ansible role to release webserver and dbserver

--- cloud-demo/Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM php:7.3-apache
-COPY *.php /var/www/html/
-RUN docker-php-ext-install mysqli
--- ws/files/process_create.php
+++ ws/files/process_create.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("172.16.0.103","root","Test123!","webtest",3306);
+$conn = mysqli_connect("172.16.0.105","root","Test123!","webtest",3306);
 
 $sql = "insert into items (title, description, created) value ('{$_POST['title']}','{$_POST['description']}', now())";
 
